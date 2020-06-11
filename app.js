3f193312d95f58b139711b75b4c8468a57f413ad
Jefferson Gomes <jefferson.mg2@hotmail.com>
2020-06-11 19:09:07 +0930

    Changed the listening port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 // Import express
 const express = require('express');
 const app = express();
-const port = 3333
+const port = process.env.PORT||3000
 
 // Import botbuilder
 const { BotFrameworkAdapter, UserState, MemoryStorage } = require('botbuilder');
